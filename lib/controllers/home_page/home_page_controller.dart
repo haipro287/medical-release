bf5bc539607b2a2f8e9df05e87ca8b6b4def90cf
an <ndangan094@gmail.com>
2021-08-12 15:03:35 +0700

    test

---
+++ lib/controllers/home_page/home_page_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController{
+  PageController pageController = PageController(initialPage: 0,keepPage:false);
+}
--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:medical_chain_mobile_ui/controllers/home_page/home_page_controller.dart';
 
 class HomePageScreen extends StatelessWidget {
+  HomePageController homePageController = Get.put(HomePageController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
