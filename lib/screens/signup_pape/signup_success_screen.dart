ee7bb0a41db559d689e64e5758f3a3c29d17c55a
Tra <Tra>
2021-09-07 10:04:50 +0700

    Merged PR 1272: fix: signup success
    fix: signup success

--- lib/screens/signup_pape/signup_success_screen.dart
+++ lib/screens/signup_pape/signup_success_screen.dart
@@ -12,93 +12,106 @@ class SignupSuccessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Stack(
-        children: [
-          Container(
-            height: screenHeight(),
-            width: screenWidth(),
-            color: Colors.white,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: getHeight(410),
-                ),
-                Text(
-                  "accountConfirm".tr,
-                  style: TextStyle(
-                    color: Color(0xFF2F3842),
-                    fontSize: getWidth(22),
-                    fontWeight: FontWeight.w600,
+    return WillPopScope(
+      onWillPop: () async {
+        var signupController = Get.put(SignupPageController());
+        var editMyAccountController = Get.put(EditMyAccountController());
+        editMyAccountController.signup.value = true;
+        editMyAccountController.email.text = signupController.email.text;
+        editMyAccountController.phone.text = signupController.phone.text;
+        editMyAccountController.avatar.value = 1;
+
+        Get.to(() => EditMyAccountScreen());
+        return true;
+      },
+      child: Material(
+        child: Stack(
+          children: [
+            Container(
+              height: screenHeight(),
+              width: screenWidth(),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: getHeight(410),
                   ),
-                ),
-                SizedBox(
-                  height: getHeight(27),
-                ),
-                Container(
-                  padding: EdgeInsets.only(
-                    left: getWidth(40),
-                    right: getWidth(40),
+                  Text(
+                    "accountConfirm".tr,
+                    style: TextStyle(
+                      color: Color(0xFF2F3842),
+                      fontSize: getWidth(22),
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  child: Text(
-                    "accountConfirmMessage".tr,
-                    style: TextStyle(fontSize: getWidth(17)),
-                    textAlign: TextAlign.center,
+                  SizedBox(
+                    height: getHeight(27),
                   ),
-                ),
-                SizedBox(
-                  height: getHeight(170),
-                ),
-                Bouncing(
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: getHeight(48),
-                    width: getWidth(343),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFD0E8FF),
-                      borderRadius: BorderRadius.circular(4),
+                  Container(
+                    padding: EdgeInsets.only(
+                      left: getWidth(40),
+                      right: getWidth(40),
                     ),
                     child: Text(
-                      "next".tr,
-                      style: TextStyle(
-                        fontSize: getWidth(17),
-                        fontWeight: FontWeight.w500,
-                      ),
+                      "accountConfirmMessage".tr,
+                      style: TextStyle(fontSize: getWidth(17)),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  onPress: () {
-                    var signupController = Get.put(SignupPageController());
-                    var editMyAccountController = Get.put(EditMyAccountController());
-                    editMyAccountController.signup.value = true;
-                    editMyAccountController.email.text = signupController.email.text;
-                    editMyAccountController.phone.text = signupController.phone.text;
-                    editMyAccountController.avatar.value = 1;
+                  SizedBox(
+                    height: getHeight(170),
+                  ),
+                  Bouncing(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: getHeight(48),
+                      width: getWidth(343),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFD0E8FF),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Text(
+                        "next".tr,
+                        style: TextStyle(
+                          fontSize: getWidth(17),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    onPress: () {
+                      var signupController = Get.put(SignupPageController());
+                      var editMyAccountController = Get.put(EditMyAccountController());
+                      editMyAccountController.signup.value = true;
+                      editMyAccountController.email.text = signupController.email.text;
+                      editMyAccountController.phone.text = signupController.phone.text;
+                      editMyAccountController.avatar.value = 1;
 
-                    Get.to(() => EditMyAccountScreen());
-                  },
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: getHeight(380),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage('assets/images/my_account_background.png'),
+                      Get.to(() => EditMyAccountScreen());
+                    },
+                  )
+                ],
               ),
             ),
-          ),
-          Container(
-            height: getHeight(380),
-            alignment: Alignment.center,
-            child: SvgPicture.asset(
-              'assets/images/confirm.svg',
-              width: getWidth(120),
-              height: getHeight(120),
+            Container(
+              height: getHeight(380),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/my_account_background.png'),
+                ),
+              ),
             ),
-          ),
-        ],
+            Container(
+              height: getHeight(380),
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/images/confirm.svg',
+                width: getWidth(120),
+                height: getHeight(120),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
